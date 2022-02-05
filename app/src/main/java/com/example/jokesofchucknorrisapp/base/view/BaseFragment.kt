f466420a4019bca87252fc969f440c0b46c2cd83
omy-q <olya_aparina@mail.ru>
2022-02-05 18:41:24 +0300

    create base presenter and fragment

---
+++ app/src/main/java/com/example/jokesofchucknorrisapp/base/view/BaseFragment.kt
@@ -0,0 +1,32 @@
+package com.example.jokesofchucknorrisapp.base.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import moxy.MvpAppCompatFragment
+
+typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
+
+abstract class BaseFragment<VB : ViewBinding>(
+    private val inflate: Inflate<VB>) : MvpAppCompatFragment() {
+
+    private var _binding: VB? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = inflate.invoke(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
